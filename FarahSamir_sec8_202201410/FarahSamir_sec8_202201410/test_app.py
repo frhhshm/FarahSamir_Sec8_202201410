f40beb0e7a2bdcb27840ad00ff9bf2c67bfeaa9d
frhhshm <154552207+frhhshm@users.noreply.github.com>
2024-12-14 00:34:30 +0200

    Add files via upload

---
+++ FarahSamir_sec8_202201410/FarahSamir_sec8_202201410/test_app.py
@@ -0,0 +1,23 @@
+import unittest
+from app import add_task, get_tasks, tasks
+
+class TestToDoApp(unittest.TestCase):
+    def setUp(self):
+
+        tasks.clear()
+
+    def test_add_task(self):
+        add_task("swe")
+        self.assertEqual(get_tasks(), ["swe"])
+
+        add_task("dl")
+        self.assertEqual(get_tasks(), ["swe", "dl"])
+
+    def test_get_tasks(self):
+        self.assertEqual(get_tasks(), [])
+
+        add_task("stat")
+        self.assertEqual(get_tasks(), ["stat"])
+
+if __name__ == "__main__":
+    unittest.main()
